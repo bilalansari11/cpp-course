605ff7b7d83ec9ed18c06f459080cdc49e21541b
Bilal Ahmed Ansari <ansarii.bilal04@gmail.com>
2026-08-07 16:23:53 +0500

    information about comments like single line comment & multiline comments, and information about variable

---
+++ video 3/tut3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// This program is created by Bilal Ansari
+/* this
+is
+a 
+multi 
+lines 
+comment */
+
+int main() {
+    int num = 6; // this is a variable 
+    cout << "Hello World " << num;
+    return 0;
+}
